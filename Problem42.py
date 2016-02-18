b842131f6394e883be58b8deefe09119b49fa33d
sspenst <spencerspenst@gmail.com>
2016-02-17 19:45:14 -0800

    Problem 42 solution

---
+++ Problem42.py
@@ -0,0 +1,25 @@
+def stringtoint(string):
+	sum = 0
+	for n in range(1, len(string) - 1):
+		sum += ord(string[n]) - 64
+	return sum
+	
+def istrinum(num):
+    n = 1
+    while 1:
+		if n * (n + 1) / 2 == num:
+			return True
+		elif n * (n + 1) / 2 > num:
+			return False
+		n += 1
+
+import re
+
+with open("Files/words.txt") as file:
+	total = 0
+	data = re.split(r"[,]", file.read())
+	for word in data:
+		if istrinum(stringtoint(word)):
+			total += 1
+
+print total
