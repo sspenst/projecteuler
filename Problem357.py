1ed320731d2f51389fcdd7bd91816dd9cacd205e
sspenst <spencerspenst@gmail.com>
2017-12-20 16:48:34 -0800

    eulerlib

--- Problem357.py
+++ Problem357.py
@@ -1,27 +1,8 @@
-def generate_primes(n):
-    """ Input n>=6, Returns a list of primes, 2 <= p < n """
-    n, correction = n-n%6+6, 2-(n%6>1)
-    sieve = [True] * int(n/3)
-    for i in range(1,int(n**0.5)/3+1):
-      if sieve[i]:
-        k=3*i+1|1
-        sieve[      k*k/3      ::2*k] = [False] * ((n/6-k*k/6-1)/k+1)
-        sieve[k*(k-2*(i&1)+4)/3::2*k] = [False] * ((n/6-k*(k-2*(i&1)+4)/6-1)/k+1)
-    return [2,3] + [3*i+1|1 for i in range(1,n/3-correction) if sieve[i]]
+import eulerlib
 
-def prime_sieve(n):
-    # Sieve of Eratosthenes
-    result = [True] * (n + 1)
-    result[0] = result[1] = False
-    for i in range(int(n**0.5 + 1)):
-        if result[i]:
-            for j in range(i * i, len(result), i):
-                result[j] = False
-    return result
+LIMIT = 10**8
 
-LIMIT = 100000000
-
-primes = prime_sieve(LIMIT + 1)
+primes = eulerlib.prime_sieve(LIMIT + 1)
 print("Primes generated")
 
 total = 0
